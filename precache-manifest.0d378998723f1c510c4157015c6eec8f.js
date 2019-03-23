ed8e3da310e07d26694d90500d5c1a5032fe822e
lorenmh <neutronstar@gmail.com>
2019-03-23 16:13:32 -0700

    first commit

---
+++ precache-manifest.0d378998723f1c510c4157015c6eec8f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "46ecad17938819c47cde",
+    "url": "/static/js/main.46ecad17.chunk.js"
+  },
+  {
+    "revision": "88097e0cb6fe8f380163",
+    "url": "/static/js/1.88097e0c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d922e3f28edef1905eb0f23f9578a300",
+    "url": "/index.html"
+  }
+];
